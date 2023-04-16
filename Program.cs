4b047f90a5a900427be15e47c4392dcfe0e13b5a
copper <79734708+OlympianGames@users.noreply.github.com>
2023-04-16 14:38:35 -0500

    Removed Output, Added Test Async

--- Program.cs
+++ Program.cs
@@ -2,82 +2,186 @@
 {
     public static Random random = new Random();
 
+    public const string NamePath = "Project Files/config/Names.txt";
+    public const string PlacePath = "Project Files/config/Places.txt";
+    public const string AgePath = "Project Files/config/Age.txt";
+    public const string HeightPath = "Project Files/config/Height.txt";
+
     public static void Main()
     {
-        for (int i = 0; i < 1000; i++)
+        GenerateCredentials(500);
+    }
+
+    public static void GenerateCredentials(int amount)
+    {
+        List<Task> tasks = new List<Task>();
+
+        for (int i = 0; i < amount; i++)
         {
-            Credentials credentials = new Credentials(RandomName(), RandomPlace(), RandomAge(), RandomHeight());
-
-            Console.WriteLine($"Name - {credentials.name}");
-            Console.WriteLine($"Place - {credentials.place}");
-            Console.WriteLine($"Birthday - {credentials.age.birthDate}");
-            Console.WriteLine($"Height - {credentials.height.height}");
-            Console.WriteLine();
-
-            SaveCredentials(credentials);
+            tasks.Add(Task.Factory.StartNew(() => { GenerateCredential();}));
         }
+
+        Task.WaitAll(tasks.ToArray());
+
+        // i copied this from stack overflow and it didn't seem needed
+        // https://stackoverflow.com/questions/8815147/run-a-method-multiple-times-simultaneously-in-c-sharp
+        //then add the result of all the tasks to r in a treadsafe fashion
+        // r = tasks.Select(task => task.Result).ToList();
+    }
+
+    public static void GenerateCredential()
+    {
+        Credentials credentials = new Credentials(RandomName(), RandomPlace(), RandomAge(), RandomHeight());
+
+        Console.WriteLine($"Name - {credentials.name}");
+        Console.WriteLine($"Place - {credentials.place}");
+        Console.WriteLine($"Birthday - {credentials.age.birthDate}");
+        Console.WriteLine($"Height - {credentials.height.height}");
+        Console.WriteLine();
+
+        SaveCredentials(credentials);
     }
 
     public static void SaveCredentials(Credentials credentials)
     {
-        using(StreamWriter writer = new StreamWriter($"Project Files/output/{credentials.name}.txt"))
+        string fileName = credentials.name;
+        string path = $"Project Files/output/{fileName}.txt";
+        try
         {
-            writer.Write(credentials.ToString());
+            // code for checking if there is duplicates, and if so deletes them.
+            // there is 22k names fuck duplicates amirgiht 
+            
+            // if(File.Exists(path))
+            // {
+            //     int duplicate = 0;
+            //     while (File.Exists($"{path} - {duplicate}"))
+            //     {
+            //         duplicate += 1;
+            //     }
+                
+            //     fileName = $"{fileName} - {duplicate}";
+            // }
+
+            using(StreamWriter writer = new StreamWriter(path))
+            {
+                writer.Write(credentials.ToString());
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 
     public static string RandomName()
     {
-        var lines = File.ReadAllLines("Project Files/config/Names.txt");
-        return lines[random.Next(0, lines.Length - 1)];
+        try
+        {
+            var lines = File.ReadAllLines(NamePath);
+            return lines[random.Next(0, lines.Length - 1)];
+        }
+        catch (System.Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return "error";
+        }
     }
 
     public static string RandomPlace()
     {
-        var lines = File.ReadAllLines("Project Files/config/Places.txt");
-        return lines[random.Next(0, lines.Length - 1)];
+        try
+        {
+            var lines = File.ReadAllLines(PlacePath);
+            return lines[random.Next(0, lines.Length - 1)];  
+        }
+        catch (System.Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            return "error";
+        }
     }
 
     public static Age RandomAge()
     {
-        var lines = File.ReadAllLines("Project Files/config/Age.txt");
+        try
+        {
+            var lines = File.ReadAllLines(AgePath);
 
-        int minAge = int.Parse(lines[1]);
-        int maxAge = int.Parse(lines[4]);
+            int minAge = int.Parse(lines[1]);
+            int maxAge = int.Parse(lines[4]);
 
-        int yearsOld = random.Next(minAge, maxAge);
-        int daysOld = yearsOld * 365;
-    
-        return new Age(daysOld);
+            int yearsOld = random.Next(minAge, maxAge);
+            int daysOld = yearsOld * 365;
+        
+            return new Age(daysOld);
+        }
+        catch (System.Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            return new Age(0);
+        }
     }
 
     public static Height RandomHeight()
     {
-        var lines = File.ReadAllLines("Project Files/config/Height.txt");
+        try
+        {
+            var lines = File.ReadAllLines(HeightPath);
 
-        int minFeet = int.Parse(lines[1]);
-        int minInches = int.Parse(lines[2]);
+            int minFeet = int.Parse(lines[1]);
+            int minInches = int.Parse(lines[2]);
 
-        int maxFeet = int.Parse(lines[5]);
-        int maxInches = int.Parse(lines[6]);
-        
-        int randomFeet = RandomInt(minFeet, maxFeet);
-        int randomInches = RandomInt(minInches, maxInches);
+            int maxFeet = int.Parse(lines[5]);
+            int maxInches = int.Parse(lines[6]);
+            
+            int randomFeet = RandomInt(minFeet, maxFeet);
+            int randomInches = RandomInt(minInches, maxInches);
 
-        return new Height(randomFeet, randomInches);
+            return new Height(randomFeet, randomInches);   
+        }
+        catch (System.Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            return new Height(0);
+        }
     }
 
     public static int RandomInt(int bottom = 0, int top = 10)
     {
-        int min = bottom;
-        int max = top;
+        try
+        {
+            int min = bottom;
+            int max = top;
 
-        if(min > max)
-            return random.Next(max, min);
+            if(min > max)
+                return random.Next(max, min);
 
-        if(max > min)
-            return random.Next(min, max);
+            else if(max > min)
+                return random.Next(min, max);
 
-        return 0;
+            return bottom;   
+        }
+        catch (System.Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Console.ForegroundColor = ConsoleColor.White;
+            
+            return bottom;
+        }
     }
 }
